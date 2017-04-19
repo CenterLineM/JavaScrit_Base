51d9eb1e9c860df5ddb13c10bce14521e13cc166
CenterLineM <joviankingeli@gmail.com>
2017-04-19 23:10:41 +0800

    練習二
    觀看陳忠誠的教學

---
+++ sum.js
@@ -0,0 +1,20 @@
+
+var sum = 0
+
+for (var i = 1; i <= 100; i++) {
+  sum = sum + i
+  console.log(sum)
+}
+console.log('1+....+100 = ' + sum)
+
+// 示範while
+
+var sum1 = 0
+
+i = 1
+while (i <= 100) {
+  sum1 = sum1 + i
+  i++
+}
+
+console.log('1+2...+100 = ' + sum1)
