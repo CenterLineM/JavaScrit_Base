8d50b206a9efad80ec66eb4ecaca0f4a26c40acc
CenterLineM <joviankingeli@gmail.com>
2017-05-19 07:59:34 +0800

    Eclipse 環境建置 與 Node.js 初階學習
    ONE

---
+++ app.js/debug.js
@@ -0,0 +1,9 @@
+// 偵錯
+
+var a = 1;
+var b = 'world';
+var c=function(x) {
+  console.log('hllo ' + x + a);
+};
+
+c(b);
---
+++ debugtest/debugtest.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+var a = 1;
+var b = 'world';
+var c=function(x) {
+  console.log('hllo ' + x + a);
+};
+
+c(b);
