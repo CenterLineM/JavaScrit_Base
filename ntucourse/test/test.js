95e6709108b5d95c230e2384b1c498be6e01766c
CenterLineM <joviankingeli@gmail.com>
2017-05-23 11:30:11 +0800

    刪除

--- ntucourse/test/test.js
+++
@@ -1,8 +0,0 @@
-var logger = require('log4js').getLogger('test.js');
-var should = require('should');
-var assert = require('assert');
-
-//======= Query data ===========
-describe('test', function() {
-
-});
