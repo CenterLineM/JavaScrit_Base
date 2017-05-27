b7a467eb92b457fbbda9090e953990c8b41136ba
CenterLineM <joviankingeli@gmail.com>
2017-05-27 17:02:05 +0800

    Node.js_Microblog 前端
    前端工程已完成

--- NodeJS/microblog/app.js
+++ NodeJS/microblog/app.js
@@ -7,13 +7,37 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-//var hello = require('./routes/hello');
+var hello = require('./routes/hello');
+var helper = require('./routes/helper');
 
 var app = express();
 
+// 使用layout
+var expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts); // add this use()
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view options', { layout:'layout.ejs' });
+
+
+
+// 修復
+// 錯誤: Error: Failed to lookup view "error" in views directory "F:\shareFrome\WebDev\JavaScrit_Base\NodeJS\microblog\views"
+// ✔ 成功
+app.use(express.static(__dirname + '../public'));
+
+// 修復錯誤嘗試
+// Error: Failed to lookup view "error" in views directory
+app.engine('ejs', require('ejs').renderFile);
+
+
+// 試試看可不可用 ejs
+//app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,11 +47,100 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
+app.use('/hello', hello);
 
-//建立路由規則
-//app.use('/hello', hello);
+// 路徑比對 新增
+
+/**
+app.get('/user/:username', function(req, res) {
+	res.send('user: ' + req.params.username);
+});
+*/
+
+// 控制權轉移範例
+var users = {
+		'byvoid': {
+			name: 'Carbo',
+			website: 'http://www.byvoid.com'
+		}
+};
+
+app.all('/user/:username', function(req, res, next) {
+	// 檢查使用者是否存在
+	if (users[req.paramsname]) {
+		next();
+	} else {
+		next(new Error(req.params.username + ' does not exist.'));
+	}
+});
+
+app.get('/user/:username', function(req, res) {
+	// 使用者一定存在 直接顯示
+	res.send(JSON.stringify(users[req.params.username]));
+});
+
+app.put('/user/:username', function(req, res) {
+	// 修改使用者資料
+	res.send('Done');
+});
+
+// ⚠
+// 部分視圖 範例
+// 沒成功有錯誤發生
+// Error: Failed to lookup view "error" in views directory
+app.get('/list', function(req, res) {
+	res.render('list', {
+		title: 'List',
+		item: [1991, 'byvoid', 'express', 'Node.js']
+	});
+});
+
+
+// 視圖幫手
+
+
+//⚠ 沒成功錯誤
+/**
+var helpers = require('express-helpers')(app);
+var util = require('util');
+
+app.helpers({
+	inspect: function(obj) {
+		return util.inspect(obj, true);
+	}
+});
+
+app.dynamicHelpers({
+	headers: function(req, res) {
+		return req.headers;
+	}
+});
+
+app.get('/helper', function(req, res) {
+	res.render('helper', {
+		title: 'Helper'
+	});
+});
+*/
+
+
+// 建立微網誌
+// 路由規畫
+// 成功修改
+app.use('/u', index);
+/*
+app.post('/post', index.post);
+app.use('/reg', index.reg);
+app.post('/reg', index.doReg);
+app.use('/login', index.login);
+app.post('/login', index.doLogin);
+app.use('/logout', index.logout);
+*/
+
+
 
 
 // catch 404 and forward to error handler
--- NodeJS/microblog/routes/index.js
+++ NodeJS/microblog/routes/index.js
@@ -8,4 +8,43 @@ router.get('/', function(req, res, next) {
 
 
 
+// 增加對應函數
 
+router.get('/u', function(req, res, next) {
+	res.send('respond with a resource');
+});
+
+// 下面的要改寫
+module.exports.user = function(req, res) {
+	
+};
+
+module.exports.post = function(req, res) {
+	
+};
+
+module.exports.reg = function(req, res) {
+	
+};
+
+module.exports.doReg = function(req, res) {
+	
+};
+
+
+module.exports.login = function(req, res) {
+	
+};
+
+
+
+module.exports.doLogin = function(req, res) {
+	
+};
+
+
+module.exports.logout = function(req, res) {
+	
+};
+
+module.exports = router;
