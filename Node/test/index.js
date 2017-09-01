8dd6264dbf683ee1a1d156186a21dbfed4bf0c3a
CenterLineM <joviankingeli@gmail.com>
2017-09-01 12:22:51 +0800

    Node.js Express 快速入門 Day 1 9-1
    快速建設Node,js 伺服器 9/1

---
+++ Node/test/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express(); //產生Express Applcation 物件
+app.get("/", function(req, res) { // 當 client 連線 server 的 根目錄 / 時 做出回應
+    res.send("<h1> Hello  NodeJS <h1> ");
+
+
+});
+
+app.get("/mypage", function(req, res) {
+    res.send(" Hi I am here.");
+});
+
+app.listen(3000, function() {
+    console.log("Server 已經啟動在 http://localhost:3000/");
+})
